4b2d35038ed432fb91d40f789ea7e45e5a8d014c
이승환 <lsh01087674117@gmail.com>
2020-11-15 10:06:12 +0900

    빈 생명주기 콜백시작

---
+++ src/test/java/hello/core/beandefinition/BeanLifecycleTest.java
@@ -0,0 +1,34 @@
+package hello.core.beandefinition;
+
+import hello.core.lifecycle.NetworkClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author 이승환
+ * @since 2020-11-15
+ */
+public class BeanLifecycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifecycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    static class LifecycleConfig {
+
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+
+
+}
